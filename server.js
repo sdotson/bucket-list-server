44b6acc1813b1d08c296089fb13f5eee0f3ef27d
sdotson <stu.dotson@gmail.com>
2016-07-13 17:30:08 -0400

    add all API endpoints

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
 'use strict';
 
 const express = require('express'),
+  bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   app = express();
 
 /*
 http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api
-
-Just an API to hit for bucket list app
-
-Categories
-POST categories -> new category
-DELETE categories/:id -> delete category
-PUT categories/:id -> update category
-GET categories -> get all categories as an array
-
-Users
-GET users -> List all users
-GET users/:id -> Get user
-POST users/:id -> Update user
-POST users -> Create new users
-DELETE users/:id -> Delete users
-
-Bucket List Items
-GET users/:id/items -> get all items for user with optional query strings
-  - category
-  - done status
-POST users/:id/items -> add new item
-
-GET users/:id/items/:item_id -> get item data
-PUT users/:id/items/:item_id -> update item data
-DELETE users/:id/items/:item_id -> delete item data
-
-*/
-
-/*
+https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 
 Data structures
 
@@ -60,6 +33,54 @@ ListItem = {
 }
 
 */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+// Categories endpoint
+router.route('/categories')
+  .get(function(req, res) {
+    res.json({message: "Just got a categories GET request"});
+  })
+  .post(function(req, res) {
+    res.json({message: "Just got a categories POST request"});
+  });
+
+router.route('/categories/:category_id')
+  .get(function(req,res) {
+    res.json({message: "Just got a categories GET request for " + req.params.category_id});
+  })
+  .put(function(req,res) {
+    res.json({message: "Just got a categories PUT request for " + req.params.category_id});
+  })
+  .delete(function(req,res) {
+    res.json({message: "Just got a categories DELETE request for " + req.params.category_id});
+  });
+
+// User items endpoints
+router.route('/users/:user_id/items')
+  .get(function(req, res) {
+    res.json({message: "Just got a items GET request for " + req.params.user_id});
+  })
+  .post(function(req, res) {
+    res.json({message: "Just got a items POST request for " + req.params.user_id});
+  });
+
+router.route('/users/:user_id/items/:item_id')
+  .get(function(req,res) {
+    res.json({message: "Just got a items GET request for item " + req.params.item_id + ", user " + req.params.user_id});
+  })
+  .put(function(req,res) {
+    res.json({message: "Just got a items PUT request for item " + req.params.item_id + ", user " + req.params.user_id});
+  })
+  .delete(function(req,res) {
+    res.json({message: "Just got a items DELETE request for item " + req.params.item_id + ", user " + req.params.user_id});
+  });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
